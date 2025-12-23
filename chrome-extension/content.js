fda82fd2a690fd7521bd7fdd0d06b63e6698c717
SaqibCod <saqibbca2021@gmail.com>
2025-12-23 21:04:48 +0530

    build extension

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,3 +1,43 @@
+function createAIButton(){
+    const button = document.createElement('div');
+    button.className = 'T-I-J-J5-Ji ao0 v7 T-I-at1 L3';
+    button.style.marginRight = '8px';
+    button.innerHTML= 'AI Reply';
+    button.setAttribute('role', 'button');
+    button.setAttribute('data-tooltip', 'Generate AI Reply');
+    return button;
+}
+function findComposeToolbar(){
+    const selectors = [
+        '.btC',
+        '.aDh',
+        '[role="toolbar"]',
+        '.gU.Up'
+    ];
+    for(const selector of selectors){
+        const toolbar = document.querySelector(selector)
+        if(toolbar){
+            return toolbar;
+        }
+        return null;
+    }
+}
+function getEmailContent(){
+    const selectors = [
+        '.h7',
+        '.a3s.ail',
+        '.gmail-quote',
+        '[role="presentation"]'
+    ];
+    for(const selector of selectors){
+        const content = document.querySelector(selector)
+        if(content){
+            return content.innerText.trim();
+        }
+        return null;
+    }
+}
+
 function injectButton(){
     const existingButton = document.querySelector(".ai_reply_button")
     if(existingButton) existingButton.remove();
@@ -7,7 +47,48 @@ function injectButton(){
         console.log("Toolbar not found");
         return;
     }
-    console.log("Toolbar found")
+    console.log("Toolbar found, creating AI button");
+    const button = createAIButton();
+    button.classList.add("ai_reply_button");
+    console.log("AI Button created");
+    button.addEventListener('click', async () => {
+        try{
+             button.innerText = 'Generating...';
+             button.disabled = true;
+
+        const emailContent = getEmailContent();
+        const response = await fetch('http://localhost:8080/api/email/generate', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ content: emailContent,
+            tone: 'professional' })
+            
+        });
+        if(!response.ok){
+            throw new Error("api request failed");
+            
+        }
+        const generatedReply = await response.text();
+        const composeBox = document.querySelector('[role="textbox"][g_editable="true"]');
+        if(composeBox){
+            composeBox.focus();
+            document.execCommand('insertText', false, generatedReply);
+        }else {
+            console.error('ComposeBox was not found');
+           }
+
+        } catch(error){
+            console.error(error);
+            alert('Failed to generate reply');
+        } finally {
+            button.innerHTML = 'AI Reply';
+            button.disabled = false;
+        }
+       
+    });
+    toolbar.insertBefore(button, toolbar.firstChild);
 }
 const observer = new MutationObserver(mutations => {
     for(const mutation of mutations){
